9f780a54851bca8c35219421387535832db8b0f4
suspenzo <enzupds@gmail.com>
2026-09-24 23:19:31 -0400

    correccion en archiov database

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,15 +1,16 @@
+require('dotenv').config();
+
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-    'ecomonitor',
-    'root',
-    '',
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-        host: 'localhost',
-        port: 3307,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
         dialect: 'mysql'
     }
 );
 
 module.exports = sequelize;
-
